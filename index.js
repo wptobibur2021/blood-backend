7707463ccc7cdbb1121fe363b8abc0ea17b64168
Md Tobibur Rohman <71486299+wptobibur2021@users.noreply.github.com>
2022-03-29 00:22:33 +0600

    Update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const app = express()
 const cors = require('cors')
 const bcrypt = require('bcrypt')
-const multer  = require('multer')
+const multer = require('multer')
 const helmet = require("helmet");
 app.use(helmet());
 app.use(cors())
@@ -19,8 +19,8 @@ const objectId = require('mongodb').ObjectId
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@blood.1bv5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function database (){
-    try{
+async function database() {
+    try {
         await client.connect();
         const database = client.db('blood')
         const donorCollection = database.collection("donner")
@@ -31,7 +31,7 @@ async function database (){
         * ======================================
         **/
         // AddDonors Volunteer or Registration
-        app.post('/api/add-volunteer', async (req,res)=>{
+        app.post('/api/add-volunteer', async (req, res) => {
             const salt = await bcrypt.genSalt(10);
             const hashedPass = await bcrypt.hash(req.body.password, salt)
             //console.log('Hash Password: ', hashedPass)
@@ -49,31 +49,51 @@ async function database (){
         })
 
         // Login Volunteer
-        app.post('/api/login', async (req,res)=>{
+        app.post('/api/login', async (req, res) => {
             try {
-                const user = await volunteerCollection.findOne({email: req.body.email})
+                const user = await volunteerCollection.findOne({ email: req.body.email })
                 !user && await res.status(400).json('User not found')
                 const validPass = await bcrypt.compare(req.body.password, user.password)
                 !validPass && await res.status(400).json('Wrong Password')
-                const {password, ...others} = user
+                const { password, ...others } = user
                 //await res.json(user)
                 await res.status(200).json(others)
-            }catch (e) {
+            } catch (e) {
                 res.status(500).json(e)
             }
         })
 
         // API Add Donors
-        app.post('/api/add-donner', async (req,res)=>{
+        app.post('/api/add-donner', async (req, res) => {
             try {
                 const newData = req.body
                 const result = await donorCollection.insertOne(newData)
                 await res.status(200).json(result)
                 console.log('New Data: ', newData)
-            }catch (e) {
+            } catch (e) {
                 console.log(e)
             }
 
+        })
+
+        app.put('/api/update-donner', async (req, res) => {
+            try {
+                const updateData = req.body
+                console.log('UserId: ', updateData)
+                const query = { _id: objectId(req.body.userId) }
+                const updateOne = {
+                    $set: {
+                        name: updateData.name,
+                        address: updateData.address,
+                        mobile: updateData.mobile,
+                        last: updateData.last
+                    },
+                }
+                const result = await donorCollection.updateOne(query, updateOne);
+                await res.status(200).json(result)
+            } catch (e) {
+
+            }
         })
 
         /**
@@ -84,76 +104,76 @@ async function database (){
         // Volunteer Photo Upload
 
         const storage = multer.diskStorage({
-            destination: (req,file, cb) =>{
+            destination: (req, file, cb) => {
                 cb(null, 'public/images')
             },
-            filename: (req,file, cb)=>{
+            filename: (req, file, cb) => {
                 cb(null, req.body.name)
             }
         })
-        const photoUpload = multer({storage});
-        app.post('/api/photoUpload', photoUpload.any("file"),(req,res)=>{
-            try{
+        const photoUpload = multer({ storage });
+        app.post('/api/photoUpload', photoUpload.any("file"), (req, res) => {
+            try {
                 return res.status(200).json("File Upload Successfully")
-            }catch (e) {
+            } catch (e) {
                 console.log(e)
             }
         })
 
-    /**
-     * =====================================
-     *  GET API DECLARATION BELOW
-     * =====================================
-     * **/
-    // GET ALL DONORS
-    app.get('/api/get-donors', async (req,res)=>{
-        const allDonors = await donorCollection.find({})
-        const result = await allDonors.toArray()
-        await res.status(200).json(result)
-    })
-    // QUERY WITH BLOOD GROUP
-    app.get('/api/query/get-donors', async (req,res)=>{
-        const group = req.query.group
-        const query = {group: group}
-        let result = []
-        if(group !== undefined){
-            result = await donorCollection.find(query).toArray()
-        }else{
-            result = await donorCollection.find({}).toArray()
-        }
-        await res.status(200).json(result)
-    })
-    // Volunteer Wise DONORS FIND OUT
-        app.get('/api/my-donors-list/:id', async (req,res)=>{
+        /**
+         * =====================================
+         *  GET API DECLARATION BELOW
+         * =====================================
+         * **/
+        // GET ALL DONORS
+        app.get('/api/get-donors', async (req, res) => {
+            const allDonors = await donorCollection.find({})
+            const result = await allDonors.toArray()
+            await res.status(200).json(result)
+        })
+        // QUERY WITH BLOOD GROUP
+        app.get('/api/query/get-donors', async (req, res) => {
+            const group = req.query.group
+            const query = { group: group }
+            let result = []
+            if (group !== undefined) {
+                result = await donorCollection.find(query).toArray()
+            } else {
+                result = await donorCollection.find({}).toArray()
+            }
+            await res.status(200).json(result)
+        })
+        // Volunteer Wise DONORS FIND OUT
+        app.get('/api/my-donors-list/:id', async (req, res) => {
             const id = req.params.id
-            const query = {userId: id}
+            const query = { userId: id }
             console.log("Query: ", query)
             const result = await donorCollection.find(query).toArray()
             console.log("result: ", result)
             await res.status(200).json(result)
 
         })
-    /**
-     * =====================================
-     *  DELETE API DECLARATION BELOW
-     * =====================================
-     * **/
-    // Delete Donor
-        app.delete('/api/delete-donor/:id', async (req,res)=>{
+        /**
+         * =====================================
+         *  DELETE API DECLARATION BELOW
+         * =====================================
+         * **/
+        // Delete Donor
+        app.delete('/api/delete-donor/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: objectId(id)}
+            const query = { _id: objectId(id) }
             console.log('Query: ', query)
             const result = await donorCollection.deleteOne(query)
             await res.status(200).json(result)
         })
-    }finally {}
+    } finally { }
 }
 database().catch(console.dir);
 
 // Root Get API
-app.get('/', async (req, res)=>{
+app.get('/', async (req, res) => {
     await res.send('Backend Server ok')
 })
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`'Backend Server Start at http://localhost:${port}`)
 })
